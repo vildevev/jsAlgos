870918ce85ca4a3a6ec6c2caff8a5178a9000208
Vilde Vevatne <v-viveva@microsoft.com>
2018-11-05 13:41:12 -0800

    updates pop

--- Stack.js
+++ Stack.js
@@ -7,7 +7,7 @@ class Stack {
     return this.storage;
   }
   pop() { // O(1)
-    this.storage = this.storage.substring(0, this.storage.indexOf(','));
+    this.storage = this.storage.slice(0, this.storage.lastIndexOf(','));
   }
   size() { // O(n)
     return this.storage.split(",").length - 1
